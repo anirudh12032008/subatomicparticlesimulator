057684a0249605c4a6c69484604f30192412e5f2
Anirudh Sahu <renudilip19@gmail.com>
2025-07-31 18:48:00 +0530

    again port

--- app.py
+++ app.py
@@ -295,4 +295,4 @@ patchmod()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0',
-                 port=5002, allow_unsafe_werkzeug=True)
+                 port=8081, allow_unsafe_werkzeug=True)
