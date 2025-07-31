41cfe9003f6b87d6b94a4e3b7f2b8ee22fe3c5a5
Anirudh Sahu <renudilip19@gmail.com>
2025-07-31 18:49:45 +0530

    finally got a free port

--- app.py
+++ app.py
@@ -295,4 +295,4 @@ patchmod()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0',
-                 port=8081, allow_unsafe_werkzeug=True)
+                 port=8005, allow_unsafe_werkzeug=True)
